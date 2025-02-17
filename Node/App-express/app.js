cb6b076e36b3080498d588da90e6f16b74016796
Vagner Henrique <vagner.lit.ifce.edu.br>
2025-02-17 19:49:57 -0300

    node2

--- Node/App-express/app.js
+++ Node/App-express/app.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
 
-// Rota para a URL raiz
+// Middleware para servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para a página inicial (GET)
 app.get('/', (req, res) => {
-  res.send('Olá, mundo com Express!');
+  // res.send('Página Inicial');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}/`);
+// Rota para a página "Sobre" (GET)
+app.get('/sobre', (req, res) => {
+  // res.send('Página Sobre');
+  res.sendFile(path.join(__dirname, 'sobre.html'));
+
+});
+
+app.get('/saiba:id',(req, res) =>{
+
+  const userId = req.params.id;
+
+  res.send(`Aqui a página Saiba mais. ${userId}`);
+}
+);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
--- Node/Exemplo 01/Server.js
+++ Node/Exemplo 01/Server.js
@@ -1,13 +1,24 @@
 
 const http = require('http'); // Módulo nativo do Node.js
 const port = 3000;
+const port2 = 3001;
 
-const server = http.createServer((req, res) => {
+const server1 = http.createServer((req, res) => {
   res.statusCode = 200; // Código de status HTTP OK
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Ola, mundo!\n'); // Resposta enviada ao cliente
+  res.end('Ola, Esse é o servidor 1\n'); // Resposta enviada ao cliente
 });
 
-server.listen(port, () => {
+const server2 = http.createServer((req, res) => {
+  res.statusCode = 200; // Código de status HTTP OK
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Ola, Esse é o servidor 2\n'); // Resposta enviada ao cliente
+});
+
+server1.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}/`);
+});
+
+server2.listen(port2, () => {
+  console.log(`Servidor 2  rodando em http://localhost:${port2}/`);
 });
