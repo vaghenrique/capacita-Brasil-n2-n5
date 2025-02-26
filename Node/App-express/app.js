fed796598cc0692e7b68bcfd99854228c04730f1
Vagner Henrique <vagner.lit.ifce.edu.br>
2025-02-26 18:25:48 -0300

    MVC2

--- Node/App-express/app.js
+++ Node/App-express/app.js
@@ -5,6 +5,11 @@ const produtoRoutes = require("./routes/produtoRoutes");
 app.set("view engine", "ejs");
 app.use("/produtos", produtoRoutes);
 
+// Definir uma rota raiz para redirecionar para /produtos
+app.get("/", (req, res) => {
+    res.redirect("/produtos");
+});
+
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
